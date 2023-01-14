1dedff83cf5f253e25155cae2e7325b092842e11
AsutoshAcharya <asutosh246@gmail.com>
2023-01-14 13:27:09 +0530

    using useQuery

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import { Route, Link, BrowserRouter, Routes } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 import "./App.css";
 import { HomePage } from "./components/Home.page";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
 import { SuperHeroesPage } from "./components/SuperHeroes.page";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Routes>
-        <Route path="/super-heroes" element={<SuperHeroesPage />} />
-        <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
