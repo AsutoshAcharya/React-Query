277cbefb84f424738c760ecc7d7214320f5bd166
AsutoshAcharya <asutosh246@gmail.com>
2023-01-14 15:58:37 +0530

    side effects using onSuccess and onError callbacks

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,18 +1,32 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
+//polling fetching data in regular intervals
+
 const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const onSuccess = () => {
+    console.log("perform side effect after data fetching");
+  };
+  const onError = () => {
+    console.log("perform side effect after encountering error");
+  };
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
     {
       // staleTime: 0,
       refetchOnMount: true,
-      refetchOnWindowFocus: false,
+      // refetchOnWindowFocus: false,
+      // refetchInterval: 3000,
+      // refetchIntervalInBackground: true,
+
+      enabled: false,
+      onSuccess,
+      onError,
     }
   );
   // console.log(isFetching);
@@ -26,9 +40,44 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
-      {data?.data.map((hero) => {
-        return <div key={hero.name}>{hero.name}</div>;
-      })}
+      <button
+        onClick={refetch}
+        style={{
+          backgroundColor: "green",
+          border: "none",
+          padding: "10px",
+          borderRadius: "10px",
+          color: "white",
+          cursor: "pointer",
+        }}
+      >
+        Fetch heroes
+      </button>
+      <br />
+      <br />
+      <div
+        style={{
+          gap: "5px",
+          display: "flex",
+        }}
+      >
+        {data?.data.map((hero) => {
+          return (
+            <div
+              key={hero.name}
+              style={{
+                backgroundColor: "green",
+                border: "none",
+                padding: "10px",
+                borderRadius: "10px",
+                color: "white",
+              }}
+            >
+              {hero.name}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
