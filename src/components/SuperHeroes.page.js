170ee227807b95a3eb7c71f76f3da6a3c544e705
AsutoshAcharya <asutosh246@gmail.com>
2023-01-14 13:44:58 +0530

    handling errors

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -4,16 +4,27 @@ import axios from "axios";
 export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [err, setErr] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:4000/superheroes").then((res) => {
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setErr(err.message);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (err) {
+    return <h2>{err}</h2>;
   }
 
   return (
